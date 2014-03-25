7dad306ba9281706789a0578205a9b0ebf82d110
cvm-new <breakthrough@cvm-new.(none)>
2014-03-24 22:53:58 -0400

    Added prototype functions for various C++ surface operations to be benchmarked.

---
+++ benchmarks/cppsurfops.cpp
@@ -0,0 +1,16 @@
+//
+//
+//
+//
+
+// extern "C"              
+// #ifdef _WIN32
+// __declspec(dllexport)
+// #endif
+
+int fill_surf(int* array_data, int array_width, int array_height);
+int fill_surf_memest(){ return 0; }
+int copy_surf(){ return 0; }
+int copy_surf_memcpy(){ return 0; }
+int rgbflag_horiz(){ return 0; }
+int rgbflag_vert(){ return 0; }
