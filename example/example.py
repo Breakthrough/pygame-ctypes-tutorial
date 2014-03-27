f1c00027a529cb784c27b4a20e5a4b525f0e9274
cvm-new <breakthrough@cvm-new.(none)>
2014-03-27 19:37:57 -0400

    Completed example Python code, and changed C++ code to use fixed-width 32-bit integers.

--- example/example.py
+++ example/example.py
@@ -1,42 +1,62 @@
 #
+# Pygame Surface Access/Writing Example - Demonstration Program
 #
+# Demonstrates creating a Pygame window, and modifying the underlying surface
+# directly with the shared library functions compiled in surfmanip.so/surfmanip.dll.
+# Note that this may need to be modified for non 32-bit ARGB surfaces.
 #
+# Part of Accessing Pygame Surfaces in C++ with ctypes and Numpy Tutorial:
+# http://github.com/Breakthrough/pygame-ctypes-tutorial | http://www.bcastell.com
 #
 
 import pygame
+import numpy
 
-import system
+import ctypes
+import sys
 import time
-
-WINDOW_RESOLUTION = (720, 480)
-SHARED_LIB_NAME   = "surfmanip"
-
+import os
 
 def load_shared_lib(libname):
+    # We assume the shared library is present in the same directory as this script.
+    libpath = os.path.dirname(os.path.realpath(__file__))
 
-    # Append proper extension to library name (.dll for Windows, .so for Linux)
-    if sys.platform in ("win32", "cli"):
-        libname = "%s.dll" % libname
+    # Append proper extension to library path (.dll for Windows, .so for Linux)
+    if sys.platform in ("win32", "cygwin"):
+        libpath = os.path.join(libpath, "%s.dll" % libname)
     else:
-        libname = "lib%s.so" % libname
+        libpath = os.path.join(libpath, "%s.so" % libname)
+
+    # Check that library exists (in same folder as this script).
+    if not os.path.exists(libpath):
+        print "Error - could not find shared library %s; could not find file:" % libname
+        print " >> %s" % libpath
+        return None
+
+    return ctypes.CDLL(libpath)
 
 
 
 def main():
 
 
-    screen = pygame.display.set_mode(WINDOW_RESOLUTION)
+    screen = pygame.display.set_mode((720, 480))
 
-    cpp_shared_lib = load_shared_lib(SHARED_LIB_NAME)
+    lib_surfmanip = load_shared_lib("surfmanip")
 
-    if not cpp_shared_lib:
-        print "Fatal error - could not load shared library %s." % SHARED_LIB_NAME
+    if not lib_surfmanip:
+        print "Fatal error - could not load surfmanip library! Exiting..."
         return
+    # Get a handle to the draw_square(...) function in our shared library.
+    draw_func = lib_surfmanip.draw_square
+    draw_func.restype = None     # Set return type (None for void, defaults to ctypes.c_int).
+    # Arguments: surf. data ptr, width, height, stride, red val, blue val, green val
+    draw_func.argtypes = [ numpy.ctypeslib.ndpointer(ctypes.c_int32),
+                           ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int32 ]
 
-
-    # Call our C++ function to modify the screen surface pixels directly.
-    #cpp_shared_lib.modify_surf(ctypes.) ...
-        
+    # Call our C++ function to draw a red (0xFF0000) box onto the screen pixels.
+    draw_func( pygame.surfarray.pixels2d(screen), screen.get_width(),
+               screen.get_height(), screen.get_pitch(), 0xFF0000 )
 
     run_main_loop = True
 
--- example/surfmanip.cpp
+++ example/surfmanip.cpp
@@ -1,24 +1,26 @@
 //
-// Pygame Surface Access/Writing Example 
+// Pygame Surface Access/Writing Example - Shared Library
+//
+// Includes functions to modify a Pygame surface (draw a square/fill a surface).
 //
 // Part of Accessing Pygame Surfaces in C++ with ctypes and Numpy Tutorial:
 // http://github.com/Breakthrough/pygame-ctypes-tutorial | http://www.bcastell.com
 //
 
+#include <cstdlib>
+
 ///
-/// fill_surface: Accepts a pointer to a 32-bit ARGB surface and draws a square in
+/// draw_square: Accepts a pointer to a 32-bit ARGB surface and draws a square in
 ///               the middle of it with the passed colour.
 ///
 extern "C"              
 #ifdef _WIN32
 __declspec(dllexport)
 #endif
-void fill_surface(unsigned char* surf_data, int surf_width, int surf_height,
-    int surf_stride, unsigned char rval, unsigned char gval, unsigned char bval)
+void draw_square(int32_t* surf_data, int surf_width, int surf_height,
+    int surf_pitch, int32_t fill_colour)
 {
     if (!surf_data) return;
-
-    unsigned char* pixel_ptr;   // Used to access the surface below.
 
     // Set the square's size to half that of the surface's smaller dimension.
     int side_len = ((surf_width < surf_height) ? surf_width : surf_height) / 2;
@@ -27,8 +29,11 @@ void fill_surface(unsigned char* surf_data, int surf_width, int surf_height,
     int start_row = (surf_height - side_len) / 2;
     int start_col = (surf_width  - side_len) / 2;
 
+    // Compute the length of a row with respect to our 32-bit pointer.
+    int row_len = surf_pitch / 4;   // 4 bytes per pixel.
+
     // Make sure we don't exceed the surface boundaries while accessing it.
-    if (   start_row < 0 || start_col < 0
+    if (   row_len <= 0 || start_row < 0 || start_col < 0
         || start_row + side_len >= surf_height
         || start_col + side_len >= surf_width )
         return;
@@ -39,16 +44,33 @@ void fill_surface(unsigned char* surf_data, int surf_width, int surf_height,
         for (int col = 0; col < (start_col + side_len); col++)
         {
             // To access a pixel, we compute the address as:
-            //   (pixel_y * stride) + (pixel_x * bpp)
-            // Where stride is the length of 1 row of the image (in bytes) in
-            // memory, and bpp is the size of a pixel (in bytes).
-            pixel_ptr = surf_data + (row * surf_stride) + (col * 4);
-            // In ARGB format, the pixel values are packed together, so we can
-            // just access each sequential byte as the next color;
-            // pixel_ptr[0] = 0xFF; // We don't touch the alpha value.
-            pixel_ptr[1] = rval;    // Red value.
-            pixel_ptr[2] = gval;    // Green value.
-            pixel_ptr[3] = bval;    // Blue vlaue.
+            //   (pixel_y * row_len) + (pixel_x * bpp)
+            surf_data[(row * row_len) + col] = fill_colour;
         }
     }
 }
+
+
+///
+/// fill_surface: Accepts a pointer to a 32-bit ARGB surface and sets every
+///               pixel to the passed colour.
+///
+extern "C"              
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+void fill_surface(int32_t* surf_data, int surf_width, int surf_height,
+    int surf_pitch, int32_t fill_colour)
+{
+    if (!surf_data) return;
+
+    // Draw the square in the middle of the image, pixel-by-pixel.
+    for (int row = 0; row < surf_height; row++)
+    {
+        for (int col = 0; col < surf_width; col++)
+        {
+            surf_data[(row * surf_pitch) + col] = fill_colour;
+        }
+    }
+}
+
